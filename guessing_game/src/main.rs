b9d7ea33fea939ea25c9411ead6dbd55273c772a
mhmad-nr <83580680+mhmad-nr@users.noreply.github.com>
2024-05-10 14:56:21 +0330

    guessing_game has finished successfully

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,19 +3,22 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("starting...");
-
-    let mut guess = String::new();
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {secret_number}");
 
-    loop {
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+    let mut tries: i32 = 5;
 
-        println!("this is {guess}");
+    loop {
+        println!("--------------------------------");
+        println!("You have try {tries} times:");
+        tries -= 1;
+
+        let mut guess = String::new();
+
+        match io::stdin().read_line(&mut guess) {
+            Ok(_) => (),
+            Err(_) => println!("Something went wrong"),
+        }
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
@@ -28,7 +31,10 @@ fn main() {
                 break;
             }
         }
-    }
+        if tries == 0 {
+            println!("You Loss 🙂");
 
-    println!("you gessed {}", guess);
+            break;
+        }
+    }
 }
