acca2fa5d490d1fa12d85a4aafbe70eaccc83bb7
mhmad-nr <83580680+mhmad-nr@users.noreply.github.com>
2024-05-13 15:06:21 +0330

    complete data type part

---
+++ rust-by-example/src/data_types.rs
@@ -0,0 +1,35 @@
+use std::io;
+
+pub fn main() {
+    // tuple
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    // array
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // exampel
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
+}
--- rust-by-example/src/main.rs
+++ rust-by-example/src/main.rs
@@ -1,9 +1,12 @@
+pub mod data_types;
 pub mod function;
 pub mod hello_word;
 pub mod looping;
+pub mod variables_mutability;
 
 pub fn main() {
     // hello_word::main();
     // looping::main();
-    function::main();
+    // variables_mutability::main();
+    data_types::main();
 }
