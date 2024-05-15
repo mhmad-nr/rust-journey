f1496e5878507d10532acb4c6f1352dd34f29245
mhmad-nr <83580680+mhmad-nr@users.noreply.github.com>
2024-05-15 18:10:59 +0330

    references and borrowing

--- rust-by-example/src/ownership.rs
+++ rust-by-example/src/ownership.rs
@@ -5,10 +5,24 @@ pub fn main() {
     // here an error is thrown
     // println!("The value of number is: {number}");
     println!("The value of number is: {second_number}");
+    
+    let s1 = String::from("hello");
+    
+    let len = calculate_length(&s1);
+    
+    // s1 is still available
+
+    println!("The length of '{}' is {}.", s1, len);
 
 
+     let mut s = String::from("hello");
+
+    change(&mut s);
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
